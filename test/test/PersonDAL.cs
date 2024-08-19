b02ae566f981797d74c572a465761cadf6cd6126
Truc Pham <121183450+Truc4p@users.noreply.github.com>
2024-08-19 15:42:05 +0700

    Add files via upload

---
+++ test/test/PersonDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesktopInfoSystem
+{
+    public class PersonDAL
+    {
+        // List to store person objects
+        private List<Person> persons = new List<Person>();
+
+        // Add a person to the list
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        // Retrieve all persons
+        public List<Person> GetAllPersons()
+        {
+            return persons;
+        }
+
+        // Retrieve persons by type
+        public List<Person> GetPersonsByType(Type type)
+        {
+            return persons.FindAll(p => p.GetType() == type);
+        }
+
+        // Update a person's information
+        public void UpdatePerson(int id, Person updatedPerson)
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                if (persons[i].ID == id)
+                {
+                    persons[i] = updatedPerson;
+                    break;
+                }
+            }
+        }
+
+        // Delete a person by ID
+        public void DeletePerson(int id)
+        {
+            persons.RemoveAll(p => p.ID == id);
+        }
+    }
+}
